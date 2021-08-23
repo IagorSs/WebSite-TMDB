f64277ffdf0850df147a1ab340af53d1a3ed54ad
IagorSs <iagor.penaranda@uol.com.br>
2021-08-23 09:30:53 -0300

    Front taking request from backend for user

--- js/userLogin.js
+++ js/userLogin.js
@@ -1,32 +1,66 @@
 if (sessionStorage.getItem('user')) window.location.replace(`/pages/user.html`);
 
 const forms = {
-  user: '',
-  pass: ''
+  login: '',
+  senha: ''
 };
 
 const userField = document.getElementById('User');
-userField.onchange = ({ target }) => forms.user = target.value;
+userField.onchange = ({ target }) => forms.login = target.value;
 
 const passField = document.getElementById('Pass');
-passField.onchange = ({ target }) => forms.pass = target.value;
+passField.onchange = ({ target }) => forms.senha = target.value;
 
 const form = document.getElementById('user-form');
 form.onsubmit = (event) => {
-  let userData;
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
+
+  const baseUrl = 'http://localhost:3000/api/v1/';
 
   switch (event.submitter.name) {
     // TODO implementar chamada na API pra retornar os dados
     case 'Login':
-      console.log('Tentando entrar...');
+      fetch(`${baseUrl}usuario/login`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(forms)
+      })
+      .then(res => {
+        if (res.status >= 400 && res.status < 600) {
+          throw new Error("Bad res from server");
+        }
+        return res.json();
+      })
+      .then(res => setUserSessionStorage(res))
+      .catch(res => console.log(res));
+
       break;
     case 'Register':
-      console.log('Tentando cadastrar...');
+      // fetch(`${baseUrl}usuario/novo`, {
+      //   method: 'POST',
+      //   headers,
+      //   body: JSON.stringify(forms)
+      // })
+      // .then(res => {
+      //   if (res.status >= 400 && res.status < 600) {
+      //     throw new Error("Bad res from server");
+      //   }
+      //   return res.json();
+      // })
+      // .then(res => setUserSessionStorage(res))
+      // .catch(res => console.log(res));
+
+      // FIXME remove this
+      setUserSessionStorage({inscricao: '123', email: 'email_test', nome: 'nomeTest', login: 'loginTest'});
+
       break;
     default:
       console.log('Inesperado');
       break;
   }
-
-  sessionStorage.setItem('user', JSON.stringify(userData));
 }
+
+const setUserSessionStorage = tokenObject => sessionStorage.setItem('user', JSON.stringify(tokenObject))
