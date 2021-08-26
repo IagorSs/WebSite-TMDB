e3d341228e854ccd00519d6d03824ea5741e31a4
IagorSs <iagor.penaranda@uol.com.br>
2021-08-26 15:00:13 -0300

    Index simulate data from api

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const categoryOption = document.createElement('option');
       categoryOption.value = category;
       categoryOption.innerText = category;
-  
+
       categories.appendChild(categoryOption);
     }))
     .catch(res => console.log(res));
@@ -37,103 +37,102 @@ let mostPopular = [];
 let totalPages;
 let currentPage = 1;
 
+const formatTwoNumber = (numb) => numb < 10 ? `0${numb}`:`${numb}`;
+const formatMinutes = (min) => `${formatTwoNumber(Math.floor(min / 60))}:${formatTwoNumber(min % 60)}`
+
 async function loadMovies() {
-  // if(categories.value != 'viewers') {
-  //   // FIXME conferir se isso daqui funciona
-  //   // FIXME colocar a categoria em questão na url
-  //   fetch(`${baseUrl}filme/getAll?page=${currentPage}`)
+  // FIXME conferir se isso daqui funciona do if
+  // FIXME colocar a categoria em questão na url do if
+  // FIXME colocar o id do usuário na url do else
+  // fetch(categories.value != 'viewers' ? `${baseUrl}filme/getAll?page=${currentPage}` : `${baseUrl}filme_assistido/getAll?page=${currentPage}`)
   //   .then(result => result.json())
-  //   .then(res => {
-  //     const obj = {
-  //       results: [{
-  //         id: 0,
-  //         categoria: 'tt',
-  //         duracao: 120,
-  //         nome: 'titulo',
-  //         produtora: 'prod',
-  //         arquivo: {},
-  //         descricao: 'siopse',
-  //         url_trailer: 'url_trailer',
-  //         image_url: 'image',
-  //         ator: {
-  //           id: 2,
-  //           nome: 'nome_ator',
-  //           nascimento: new Date
-  //         },
-  //         diretor: {
-  //           id: 3,
-  //           nome: 'nome_diretor',
-  //           nascimento: new Date,
-  //           filmesDirigidos: 20
-  //         }
-  //       }],
-  //       qtdePaginas: 2
-  //     }
-
-  //     res.results.map((result) => {
-
-  //       const filmeWrap = document.createElement("div");
-  //       filmeWrap.classList.add("filmeContent");
-  //       filmeWrap.onclick = () => loadTrailler(result.url_trailer);
-
-  //       const filmeImg = document.createElement("img");
-  //       filmeImg.src = result.image_url;
-  //       filmeImg.alt = `Capa de ${result.nome}`;
-  //       result.genre_ids.map((genre) => {
-  //         filmeWrap.classList.add(genre);
-  //       })
-
-  //       filmeWrap.id = String(result.id);
-  //       filmeWrap.appendChild(filmeImg);
-
-  //       mostPopular.push(filmeWrap);
-  //     })
-
-  //     totalPages = res.total_pages;
-
-  //     loadMore();
+  //   .then((res_) => {
   //   });
-  // } else {
-  //   // FIXME colocar o id do usuário na url
-  //   fetch(`${baseUrl}filme_assistido/getAll?page=${currentPage}`)
-  //   .then(result => result.json())
-  //   .then(data => {
-  //     data.results.map((result) => {
-
-  //       const capa = document.createElement("div");
-  //       capa.classList.add("capa");
-
-  //       const capaImg = document.createElement("img");
-  //       capaImg.onclick= () => window.location = `/pages/movie.html?id=${result.id}`;
-  //       capaImg.src = result.poster_path ? `https://image.tmdb.org/t/p/w200${result.poster_path}`:'/imagens/not_image.png';
-  //       capaImg.alt = `Capa de ${result.title}`;
-  //       capaImg.style.cursor = "pointer";
-  //       result.genre_ids.map((genre) => {
-  //         capa.classList.add(genre);
-  //       })
-
-  //       capa.id = String(result.id);
-  //       capa.appendChild(capaImg);
-
-  //       mostPopular.push(capa);
-  //     })
-
-  //     totalPages = data.total_pages;
-
-  //     loadMore();
-  //   });
-  // }
+  const dataTest = {
+    results: [{
+      id: 0,
+      categoria: 'tt',
+      duracao: 125,
+      nome: 'titulo',
+      produtora: 'prod',
+      arquivo: {},
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a sapien elit. Vivamus consequat sagittis justo, et egestas massa luctus vel. Phasellus varius ac nisi sit amet tempus. Suspendisse consequat, erat at dapibus egestas, erat ipsum iaculis nunc, non finibus leo libero ac risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas ultricies lacus at facilisis pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a sapien elit. Vivamus consequat sagittis justo, et egestas massa luctus vel. Phasellus varius ac nisi sit amet tempus. Suspendisse consequat, erat at dapibus egestas, erat ipsum iaculis nunc, non finibus leo libero ac risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas ultricies lacus at facilisis pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      url_trailer: 'url_trailer',
+      image_url: '/images/not_image.png',
+      ator: {
+        id: 2,
+        nome: 'nome_ator',
+        nascimento: new Date
+      },
+      diretor: {
+        id: 3,
+        nome: 'nome_diretor',
+        nascimento: new Date,
+        filmesDirigidos: 20
+      }
+    }],
+    qtdePaginas: 1
+  }
   
+  dataTest.results.map((result) => {
+
+    const filmeContent = document.createElement("div");
+    filmeContent.classList.add("filmeContent");
+    filmeContent.onclick = () => loadTrailler(result.url_trailer);
+
+    const filmeImg = document.createElement("img");
+    filmeImg.src = result.image_url;
+    filmeImg.alt = `Capa de ${result.nome}`;
+
+    filmeContent.appendChild(filmeImg);
+
+    const textContent = document.createElement('div');
+    textContent.classList.add("textContent");
+
+    const h2Title = document.createElement('h2');
+    h2Title.innerText = result.nome;
+    textContent.appendChild(h2Title);
+
+    const duration = document.createElement('p');
+    duration.innerHTML = `<b>Duração:</b> ${formatMinutes(result.duracao)}`;
+    textContent.appendChild(duration);
+
+    const producer = document.createElement('p');
+    producer.innerHTML = `<b>Produtora:</b> ${result.produtora}`
+    textContent.appendChild(producer);
+
+    const director = document.createElement('p');
+    director.innerHTML = `<b>Diretor:</b> ${result.diretor.nome}`;
+    textContent.appendChild(director);
+
+    const ator = document.createElement('p');
+    ator.innerHTML = `<b>Ator Destaque:</b> ${result.ator.nome}`;
+    textContent.appendChild(ator);
+
+    const sinopse = document.createElement('p');
+    sinopse.innerHTML = `<b>Sinopse:</b> ${result.descricao}`;
+    textContent.appendChild(sinopse);
+
+    filmeContent.appendChild(textContent);
+
+    WrapperFilmes.appendChild(filmeContent);
+
+    // mostPopular.push(filmeContent);
+  })
+
+  totalPages = dataTest.qtdePaginas;
+
+  loadMore();
 }
 
 async function loadMore() {
   let foundItems = 0;
 
-  for(let i=startMovies; i<finalMovies; i++) {
-    if(i >= mostPopular.length) {
+  for (let i = startMovies; i < finalMovies; i++) {
+    if (i >= mostPopular.length) {
 
-      if(currentPage === totalPages){
-        loadMoreButton.onclick = () => {};
+      if (currentPage === totalPages) {
+        loadMoreButton.onclick = () => { };
         return;
       }
 
@@ -181,5 +180,5 @@ showMovieButton.onclick = () => {
 };
 
 FilmSelectedOverlay.onclick = e => {
-  if(e.target == FilmSelectedOverlay) FilmSelectedOverlay.style.display = 'none';
+  if (e.target == FilmSelectedOverlay) FilmSelectedOverlay.style.display = 'none';
 }
