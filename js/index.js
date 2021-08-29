dd04da6209df20294933afa96687178b2db94cb5
IagorSs <iagor.penaranda@uol.com.br>
2021-08-29 01:13:32 -0300

    index call backend of movies

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@ const baseUrl = 'http://localhost:3000/api/v1/';
 
 const categories = document.getElementById("barra_pesquisa_id");
 
+const user = JSON.parse(sessionStorage.getItem('user'));
+
 document.addEventListener('DOMContentLoaded', () => {
-  fetch(`${baseUrl}/categoria/getAll`)
+  fetch(`${baseUrl}categoria_filme/getAll`)
     .then(res => {
       if (res.status >= 400 && res.status < 600) {
         throw new Error("Bad res from server");
@@ -20,68 +22,36 @@ document.addEventListener('DOMContentLoaded', () => {
     .catch(res => console.log(res));
 });
 
-categories.onchange = resetMovies;
+const loadCategory = (value) => {
+  let apiPath = value === 'Assistidos' ? `filmeAssistidoPor/${user.inscricao}`:`filme/getCategoria/${value}`;
+  
+  fetch(`${baseUrl}${apiPath}`)
+    .then(res => res.json())
+    .then(res => resetMovies(res));
+}
+
+categories.onchange = ({target}) => loadCategory(target.value);
 
 const WrapperFilmes = document.getElementById("WrapperFilmes");
 
-let startMovies = 0, finalMovies = 4;
-
-function resetMovies() {
-  startMovies = 0;
-  finalMovies = 4;
+function resetMovies(movies) {
   WrapperFilmes.innerHTML = '';
-  loadMore();
-}
-
-let mostPopular = [];
-let totalPages;
-let currentPage = 1;
+  loadMovies(movies);
+};
 
 const formatTwoNumber = (numb) => numb < 10 ? `0${numb}`:`${numb}`;
 const formatMinutes = (min) => `${formatTwoNumber(Math.floor(min / 60))}:${formatTwoNumber(min % 60)}`
 
-async function loadMovies() {
-  // FIXME conferir se isso daqui funciona do if
-  // FIXME colocar a categoria em questão na url do if
-  // FIXME colocar o id do usuário na url do else
-  // fetch(categories.value != 'viewers' ? `${baseUrl}filme/getAll?page=${currentPage}` : `${baseUrl}filme_assistido/getAll?page=${currentPage}`)
-  //   .then(result => result.json())
-  //   .then((res_) => {
-  //   });
-  const dataTest = {
-    results: [{
-      id: 0,
-      categoria: 'tt',
-      duracao: 125,
-      nome: 'teste dinamico',
-      produtora: 'prod',
-      arquivo: {},
-      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a sapien elit. Vivamus consequat sagittis justo, et egestas massa luctus vel. Phasellus varius ac nisi sit amet tempus. Suspendisse consequat, erat at dapibus egestas, erat ipsum iaculis nunc, non finibus leo libero ac risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas ultricies lacus at facilisis pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a sapien elit. Vivamus consequat sagittis justo, et egestas massa luctus vel. Phasellus varius ac nisi sit amet tempus. Suspendisse consequat, erat at dapibus egestas, erat ipsum iaculis nunc, non finibus leo libero ac risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas ultricies lacus at facilisis pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      url_trailer: 'FDNNHh7TRN0',
-      image_url: '/images/not_image.png',
-      ator: {
-        id: 2,
-        nome: 'nome_ator',
-        nascimento: new Date
-      },
-      diretor: {
-        id: 3,
-        nome: 'nome_diretor',
-        nascimento: new Date,
-        filmesDirigidos: 20
-      }
-    }],
-    qtdePaginas: 1
-  }
+async function loadMovies(movies) {
   
-  dataTest.results.forEach((result) => {
+  movies.forEach((result) => {
 
     const filmeContent = document.createElement("div");
     filmeContent.classList.add("filmeContent");
     filmeContent.onclick = () => loadOverlay(result);
 
     const filmeImg = document.createElement("img");
-    filmeImg.src = result.image_url;
+    filmeImg.src = result.imageUrl;
     filmeImg.alt = `Capa de ${result.nome}`;
 
     filmeContent.appendChild(filmeImg);
@@ -102,11 +72,11 @@ async function loadMovies() {
     textContent.appendChild(producer);
 
     const director = document.createElement('p');
-    director.innerHTML = `<b>Diretor:</b> ${result.diretor.nome}`;
+    director.innerHTML = `<b>Diretor:</b> ${result.idDiretor.nome}`;
     textContent.appendChild(director);
 
     const ator = document.createElement('p');
-    ator.innerHTML = `<b>Ator Destaque:</b> ${result.ator.nome}`;
+    ator.innerHTML = `<b>Ator Destaque:</b> ${result.idAtorPrincipal.nome}`;
     textContent.appendChild(ator);
 
     const sinopse = document.createElement('p');
@@ -116,43 +86,10 @@ async function loadMovies() {
     filmeContent.appendChild(textContent);
 
     WrapperFilmes.appendChild(filmeContent);
-
-    // mostPopular.push(filmeContent);
   })
-
-  totalPages = dataTest.qtdePaginas;
-
-  loadMore();
 }
 
-async function loadMore() {
-  let foundItems = 0;
-
-  for (let i = startMovies; i < finalMovies; i++) {
-    if (i >= mostPopular.length) {
-
-      if (currentPage === totalPages) {
-        loadMoreButton.onclick = () => { };
-        return;
-      }
-
-      startMovies = finalMovies + foundItems - 6;
-
-      currentPage++;
-      return loadMovies();
-    }
-
-    await addCapa(i);
-  }
-
-  startMovies = finalMovies;
-  finalMovies += 4;
-}
-
-document.addEventListener('DOMContentLoaded', loadMovies);
-
-const loadMoreButton = document.getElementById("botao_filmes");
-loadMoreButton.onclick = loadMore;
+document.addEventListener('DOMContentLoaded', () => loadCategory(categories.value));
 
 const FilmSelectedOverlay = document.getElementById('FilmSelectedOverlay');
 const loadOverlay = (filme) => {
@@ -168,14 +105,36 @@ const loadOverlay = (filme) => {
   iframe.width='80%';
   iframe.height='60%';
   iframe.allowFullscreen=true;
-  iframe.src=`https://www.youtube.com/embed/${filme.url_trailer}`;
+  iframe.src=`https://www.youtube.com/embed/${filme.videoUrl}`;
   FilmSelectedOverlay.appendChild(iframe);
+
+  const buttonsDiv = document.createElement('div');
+  buttonsDiv.classList.add('overlay-buttons');
+
+  const buttonFilmeEdit = document.createElement('button');
+  buttonFilmeEdit.classList.add('primary-button');
+  buttonFilmeEdit.innerText='Editar Filme';
+  buttonFilmeEdit.onclick= () => window.location.replace(`/pages/editFilm.html?id=${filme.id}`);
 
   const buttonFilme = document.createElement('button');
   buttonFilme.classList.add('primary-button');
   buttonFilme.innerText='Ir para o Filme';
   buttonFilme.onclick= showFilme;
-  FilmSelectedOverlay.appendChild(buttonFilme);
+
+  const buttonFilmeDelete = document.createElement('button');
+  buttonFilmeDelete.classList.add('primary-button');
+  buttonFilmeDelete.innerText='Deletar Filme';
+  buttonFilmeDelete.onclick= () => {
+    fetch(`${baseUrl}filme/deletar/${filme.id}`, { method: 'DELETE' })
+      .then(() => alert('Filme deletado'))
+      .catch(() => alert('Houver um erro'))
+  };
+
+  buttonsDiv.appendChild(buttonFilmeEdit);
+  buttonsDiv.appendChild(buttonFilme);
+  buttonsDiv.appendChild(buttonFilmeDelete);
+
+  FilmSelectedOverlay.appendChild(buttonsDiv);
 }
 
 const filmDiv = document.getElementById('ShowFilm');
