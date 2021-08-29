59d7f44c6e07018bf7260e07f99bb056fa7fe89f
IagorSs <iagor.penaranda@uol.com.br>
2021-08-29 15:53:41 -0300

    Register actor connect with backend

--- js/registerActor.js
+++ js/registerActor.js
@@ -1,17 +1,39 @@
 const form = {
-  name: '',
-  birthday: ''
-}
+  nome: '',
+  nascimento: ''
+};
+
+const baseUrl = 'http://localhost:3000/api/v1/';
 
 const nameField = document.getElementById('Name');
-nameField.onchange = ({target}) => form.name = target.value;
+nameField.onchange = ({target}) => form.nome = target.value;
 
 const birthdayField = document.getElementById('Birthday');
-birthdayField.onchange = ({target}) => form.birthday = target.value;
+birthdayField.onchange = ({target}) => form.nascimento = target.value;
 
 const formTag = document.getElementById('UpdatedForm');
 formTag.onsubmit = (e) => {
   e.preventDefault();
-  // TODO submeter form
-  console.log(form);
+
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
+
+  fetch(`${baseUrl}ator/novo`, {
+    method:'POST',
+    headers,
+    body: JSON.stringify(form)
+  })
+  .then(res => {
+    if (res.status >= 400 && res.status < 600) {
+      throw new Error("Bad res from server");
+    }
+    alert('Ator cadastrado com sucesso!');
+    window.location.reload();
+  })
+  .catch(res => {
+    alert('Houve um erro');
+    console.log(res);
+  });
 }
