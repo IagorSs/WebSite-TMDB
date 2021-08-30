e65bd076916353b3964931a1958334d44585bc3d
IagorSs <iagor.penaranda@uol.com.br>
2021-08-30 00:30:46 -0300

    update global to change to correct path

--- js/global.js
+++ js/global.js
@@ -1,1 +1,1 @@
-if (!sessionStorage.getItem('user')) window.location = 'http://127.0.0.1:5500/pages/userLogin.html';
+if (!sessionStorage.getItem('user')) window.location.replace('/pages/userLogin.html');
